a3c75eb02303fa2b771ff3bb805996fe6914c29d
aaronshivers <aaron@moonmail.org>
2020-03-01 11:00:27 -0600

    add error handling

--- express-server/src/app.js
+++ express-server/src/app.js
@@ -4,7 +4,9 @@ if (process.env.NODE_ENV !== 'production') {
 
 const express = require('express')
 const app = express()
+const helmet = require('helmet')
 const cors = require('cors')
+const timeout = require('connect-timeout')
 
 // connect to database
 require('./db/mongoose')()
@@ -12,10 +14,20 @@ require('./db/mongoose')()
 const indexRoutes = require('./routes/index/index')
 const notesRoutes = require('./routes/notes/notes')
 
+app.use(helmet())
 app.use(cors())
 app.use(express.json())
+app.use(timeout('5s'))
 
 app.use(indexRoutes)
 app.use(notesRoutes)
+
+app.use((req, res, next) => {
+  res.status(404).json({ 'error': 'Sorry, we cannot find that!' })
+})
+
+app.use((err, req, res, next) => {
+  res.status(500).json({ 'error': err.message })
+})
 
 module.exports = app
