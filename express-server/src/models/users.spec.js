8f21a6c8121dd18a97112d6b94dd81b4437f4ee4
aaronshivers <aaron@moonmail.org>
2020-03-07 22:04:00 -0600

    test user.generateAuthToken()

---
+++ express-server/src/models/users.spec.js
@@ -0,0 +1,26 @@
+const expect = require('expect')
+const jwt = require('jsonwebtoken')
+const { ObjectId } = require('mongodb')
+const User = require('../models/users')
+
+describe('users model', () => {
+
+  describe('user.generateAuthToken()', () => {
+
+    it('should return a valid JWT', async () => {
+
+      const payload = {
+        _id: new ObjectId().toHexString(),
+        email: 'user@email.com',
+        password: 'asdfASDF1234!@#$',
+      }
+
+      const user = await new User(payload)
+      const token = await user.generateAuthToken()
+      const secret = process.env.JWT_SECRET
+      const decoded = jwt.verify(token, secret)
+
+      expect(decoded._id).toEqual(payload._id)
+    })
+  })
+})
